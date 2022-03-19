b21c879a087c2308046093e735582e83fbb30708
Siddhant <santoshshrivastava39@gmail.com>
2022-03-19 16:14:03 +0530

    List Added

---
+++ Rice.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryDataManagementDemo
+{
+    public class Rice
+    {
+        public List<TypesOfRice> typesOfRice { get; set; }
+        public List<TypesOfRice> typesOfGrains { get; set; }
+        public List<TypesOfRice> typesOfwheats { get; set; }
+
+    }
+    public class TypesOfRice
+    {
+        public string Name { get; set; }
+        public string  weight { get; set; }
+        public string Price { get; set; }
+
+    }
+    public class TypesOfGrains
+    {
+        public string Name { get; set; }
+        public string weight { get; set; }
+        public string Price { get; set; }
+    }
+    public class TypesOfwheats
+    {
+        public string Name { get; set; }
+        public string weight { get; set; }
+        public string Price { get; set; }
+
+    }
+}
