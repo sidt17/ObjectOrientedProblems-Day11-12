f7e11bba8455f6f5b38f26a87788852b3df8a9cf
Siddhant <santoshshrivastava39@gmail.com>
2022-03-19 16:12:20 +0530

    Rice

---
+++ FetchDataForRice.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+using System.Threading.Tasks;
+
+namespace InventoryDataManagementDemo
+{
+    public class fetchdataforrice
+    {
+        public Rice Read(string Path)
+        {
+            using (StreamReader file = new StreamReader(Path))
+            {
+                try
+                {
+                    string json = file.ReadToEnd();
+                    Rice rice= JsonConvert.DeserializeObject<Rice>(json);
+                    return rice;
+                }
+                catch (Exception e)
+                {
+                    return null;
+                }
+            }
+        }
+
+    }
+}
