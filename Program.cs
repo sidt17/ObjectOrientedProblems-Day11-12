3a92a334e53f54b8038f98b4cb65d2c148f31ec9
Siddhant <santoshshrivastava39@gmail.com>
2022-03-19 16:36:49 +0530

    UC-2 Solved

--- Program.cs
+++ Program.cs
@@ -14,7 +14,11 @@ namespace InventoryDataManagementDemo
                 Console.WriteLine(data.typesOfRice[i].Name);
                 Console.WriteLine(data.typesOfRice[i].weight);
                 Console.WriteLine(data.typesOfRice[i].Price);
-                
+                int value = data.typesOfRice[i].weight * data.typesOfRice[i].Price;
+
+                Console.WriteLine("The value of " + data.typesOfRice[i].Name + " Rice Inventory is " + value);
+                Console.WriteLine();
+
             }
             Console.WriteLine("---------------------");
 
@@ -23,7 +27,12 @@ namespace InventoryDataManagementDemo
                 Console.WriteLine(data.typesOfGrains[i].Name);
                 Console.WriteLine(data.typesOfGrains[i].weight);
                 Console.WriteLine(data.typesOfGrains[i].Price);
-               
+
+                int value = data.typesOfGrains[i].weight * data.typesOfGrains[i].Price;
+
+                Console.WriteLine("The value of " + data.typesOfGrains[i].Name + " Grain Inventory is " + value);
+                Console.WriteLine();
+
             }
             Console.WriteLine("---------------------");
 
@@ -32,7 +41,12 @@ namespace InventoryDataManagementDemo
                 Console.WriteLine(data.typesOfwheats[i].Name);
                 Console.WriteLine(data.typesOfwheats[i].weight);
                 Console.WriteLine(data.typesOfwheats[i].Price);
-               
+
+                int value = data.typesOfwheats[i].weight * data.typesOfwheats[i].Price;
+
+                Console.WriteLine("The value of " + data.typesOfwheats[i].Name + " Wheat Inventory is " + value);
+                Console.WriteLine();
+
             }
 
 
